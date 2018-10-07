b41feb6cb7c52374e9fab99a6f2ebc839e692d50
strangehill <strangehill@antarg.co>
2018-10-08 02:34:21 +0800

    add and apply Styled Components
    `npm i styled-components gatsby-plugin-styled-components babel-plugin-styled-components`

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
+import styled from "styled-components"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+
+const Title = styled.h3`
+  margin-bottom: ${rhythm(1 / 4)};
+`
+const StyledLink = styled(Link)`
+  box-shadow: none;
+`
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -19,15 +26,9 @@ const BlogIndex = ({ data, location }) => {
         return (
           <article key={node.fields.slug}>
             <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
+              <Title>
+                <StyledLink to={node.fields.slug}>{title}</StyledLink>
+              </Title>
               <small>{node.frontmatter.date}</small>
             </header>
             <section>
