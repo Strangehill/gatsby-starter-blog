4368da1ec6002d19dbeabed07e88e60267ed32e4
strangehill <strangehill@antarg.co>
2019-03-05 13:59:28 +0800

    extract blog listing from index into separate page

---
+++ src/pages/blog.js
@@ -0,0 +1,82 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import styled from "styled-components"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+const H1 = styled.h1`
+  margin-top: 0;
+`
+
+const H3 = styled.h3`
+  margin-bottom: ${rhythm(1 / 4)};
+`
+
+const StyledLink = styled(Link)`
+  box-shadow: none;
+`
+
+const Hr = styled.hr`
+  margin-bottom: ${rhythm(1)};
+`
+
+class BlogPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="All posts"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <H1>Blog Posts</H1>
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <H3>
+                <StyledLink to={node.fields.slug}>{title}</StyledLink>
+              </H3>
+              <small>{node.frontmatter.date}</small>
+              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            </div>
+          )
+        })}
+        <Hr />
+        <Bio />
+      </Layout>
+    )
+  }
+}
+
+export default BlogPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
