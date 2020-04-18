68848a09c49cb4b7cb65ec1669b94ac594ed3520
strangehill <strangehill@antarg.co>
2020-04-18 16:39:48 +0800

    [WIP] coud've u-turn'd here

---
+++ src/pages/index.en.tsx
@@ -0,0 +1,99 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+type Data = {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        excerpt: string
+        frontmatter: {
+          title: string
+          date: string
+          description: string
+        }
+        fields: {
+          slug: string
+        }
+      }
+    }[]
+  }
+}
+
+const Title = styled.h3`
+  margin-bottom: ${rhythm(1 / 4)};
+`
+const StyledLink = styled(Link)`
+  box-shadow: none;
+`
+
+const BlogIndex = ({ data, location }: PageProps<Data>) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <Title>
+                <StyledLink to={node.fields.slug}>{title}</StyledLink>
+              </Title>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      limit: 1
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
